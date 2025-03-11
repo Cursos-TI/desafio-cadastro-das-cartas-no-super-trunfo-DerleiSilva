3c7acff78563a6928eb0eaca5b6940ba359b8ade
DerleiSilva <94967980+DerleiSilva@users.noreply.github.com>
2025-03-11 20:59:46 +0000

    Modifiquei a estrutura do programa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -44,12 +44,9 @@ int main() {
  
      printf("Digite quantos pontos turisticos existem na cidade: \n");
      scanf("%d", &ponto_Turistico_carta1);
-     printf("------------------------------------------------------------------\n");
+     printf("------------------------------------------------------------------\n\n");
 
-
-    
-
-     // Cadastro das Cartas:
+    // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     // Entrada de dados pelo ususario para carta 2
@@ -76,10 +73,8 @@ int main() {
 
     printf("Digite quantos pontos turisticos existem na cidade: \n");
     scanf("%d", &ponto_Turistico_carta2);
-    printf("------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------\n\n");
 
-
-  
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
